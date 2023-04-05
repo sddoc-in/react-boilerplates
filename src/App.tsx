ce010dd3d0fad60697a565069a5872004732810d
vinay876 <singhshahab2001@gmail.com>
2023-04-06 00:30:23 +0530

    configured

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-  <>
-  <div className="grid grid-cols-10 gap-2">
-  <div className="bg-sky-50 aspect-square"></div>
-  <div className="bg-sky-100 aspect-square"></div>
-  <div className="bg-sky-200 aspect-square"></div>
-  <div className="bg-sky-300 aspect-square"></div>
-  <div className="bg-sky-400 aspect-square"></div>
-  <div className="bg-sky-500 aspect-square"></div>
-  <div className="bg-sky-600 aspect-square"></div>
-  <div className="bg-sky-700 aspect-square"></div>
-  <div className="bg-sky-800 aspect-square"></div>
-  <div className="bg-sky-900 aspect-square"></div>
-</div>
-
-  </>
+    <>
+      {/* @ts-ignore */}
+      <div className="radial-progress text-primary" style={{ "--value": 70 }}>70%</div>
+    </>
   );
 }
 
