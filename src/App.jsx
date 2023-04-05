b84a62e373a626ac53bcac25166990d0ee9f6cce
vinay876 <singhshahab2001@gmail.com>
2023-04-06 01:14:54 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,7 @@ import React from "react";
 function App() {
     return (
         <>
-            <div class="grid grid-cols-10 gap-2">
-                <div class="bg-sky-50 aspect-square"></div>
-                <div class="bg-sky-100 aspect-square"></div>
-                <div class="bg-sky-200 aspect-square"></div>
-                <div class="bg-sky-300 aspect-square"></div>
-                <div class="bg-sky-400 aspect-square"></div>
-                <div class="bg-sky-500 aspect-square"></div>
-                <div class="bg-sky-600 aspect-square"></div>
-                <div class="bg-sky-700 aspect-square"></div>
-                <div class="bg-sky-800 aspect-square"></div>
-                <div class="bg-sky-900 aspect-square"></div>
-            </div>
+         <div className="radial-progress text-primary" style={{"--value":70}}>70%</div>
         </>
     );
 }
