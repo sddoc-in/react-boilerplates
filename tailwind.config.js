2319291c8aae407e3f28b353d44027775b298f68
vinay876 <singhshahab2001@gmail.com>
2023-04-06 01:18:29 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,5 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui"), require("@tailwindcss/typography")],
+    plugins: [ require("@tailwindcss/typography")],
 }
