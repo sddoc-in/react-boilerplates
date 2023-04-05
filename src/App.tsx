a7b45622c6070c9a4abb60cef85ba03610013c82
vinay876 <singhshahab2001@gmail.com>
2023-04-05 23:46:01 +0530

    configured

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+    {/* @ts-ignore */}
+    <div className="radial-progress bg-primary text-primary-content border-4 border-primary" style={{"--value":70}}>70%</div>
+    </>
+  );
+}
+
+export default App;
